0bb5f829dd6737b68dea77a6f214cbbee8fac375
Shreeraam Alagarsamy Sethuraj <shreeraaam@outlook.com>
2021-04-16 08:23:31 +0200

    added the function details

--- amazon_web_scraping.py
+++ amazon_web_scraping.py
@@ -1,3 +1,4 @@
+## Import Libraries.
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -8,6 +9,18 @@ asins = []
 
 
 def getasin(url):
+
+    '''
+    This function is used to
+    get the asin of all products
+    int he given url
+
+    This function accepts url as an
+    arguement.
+
+    this function returns a 
+    list of asins.
+    '''
     r = s.get(url)
     r.html.render(sleep=1)
     products = r.html.find('div[data-asin]')
@@ -19,6 +32,19 @@ def getasin(url):
 
 
 def getproductdetails(asin_list):
+
+    '''
+    This function fetches details 
+    from the asin which is stored in as
+    a list.
+
+    This function takes a list with asins from 
+    def getasin(url)
+
+    this function returns a dict with 
+    name,price,rating,review_count,soldby,
+    brand
+    '''
     for items in asin_list:
         url = f'https://www.amazon.in/dp/{items}'
         r = s.get(url)
@@ -66,6 +92,16 @@ def getproductdetails(asin_list):
 
 
 def output(data):
+
+    '''
+    this function is used to 
+    convert the dict 
+    from the getproductdetails
+    to DataFrame.
+
+    this function accepts the dict as the 
+    arguement
+    '''
     df = pd.DataFrame(data)
     print(df.shape)
     df.to_csv("spirulina_new.csv", index=False)
