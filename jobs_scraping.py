e60c7754d451289cf1d72fa31c884bf8f0042c8d
Shreeraam Alagarsamy Sethuraj <shreeraaam@outlook.com>
2021-05-10 13:16:48 +0200

    second commit

--- jobs_scraping.py
+++ jobs_scraping.py
@@ -2,52 +2,65 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
 import re
+from dateutil import parser
+import pandas as pd
+import time
 driver = webdriver.Chrome()
+records = []
 
 
-def get_data(url):
+def get_data(url): 
     driver.get(url)
+    time.sleep(4)
     content = driver.page_source
+    time.sleep(4)
     soup = BeautifulSoup(content)
     return soup
 
 
 def get_details(soup):
+    global location_
     listings = soup.find_all('li', {'class': 'ais-Hits-list-item'})
     for i in listings:
         company = i.find(class_="ais-Highlight-nonHighlighted").get_text().strip()
         post = i.find(class_="sc-1kkiv1h-9 sc-7dlxn3-4 ivyJep iXGQr").get_text().strip()
         contract = i.find(class_="sc-1qc42fc-2 bzTNsD").get_text().strip()
         try:
-            location = i.find(class_="sc-1qc42fc-2 fHAhLi").get_text().strip()
+            location_ = i.find(class_="sc-1qc42fc-2 fHAhLi").get_text().strip()
         except Exception as e:
             #raise e
             b = 0
         job_link = "https://www.welcometothejungle.com" + i.find('a')['href']
-
+        for j in i.findAll('time'):
+            if j.has_attr('datetime'):
+                dt = j['datetime']
+                date = parser.parse(dt).strftime('%B %d, %Y')
         log = {
             "company_name": company,
             "position": post,
             "contract": contract,
-            "location": location,
-            "job_link": job_link
-        }
-        listings_list.append(log)
+            "location": location_,
+            "job_link": job_link,
+            "post_date": date
+            }
+        records.append(log)
+    return
 
 
 def get_pages(soup):
     try:
-        url = 'https://www.welcometothejungle.com/fr/jobs' + soup.find('a',{'aria-label': "Next page"}).get('href')
+        url = 'https://www.welcometothejungle.com/fr/jobs' + soup.find('a', {'aria-label': "Next page"}).get('href')
         return url
     except AttributeError:
-        return None
+        return
 
 
 url = "https://www.welcometothejungle.com/fr/jobs?page=1&aroundQuery=&query=data%20engineer%20&refinementList%5Blanguage%5D%5B%5D=en"
-listings_list = []
 while True:
     soup = get_data(url)
+    get_details(soup)
     url = get_pages(soup)
     if not url:
         break
-    print(url)
+    else:
+        print(url)
